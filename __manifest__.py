b6b402a36628d0a5aae3c5f17bbdb8d9b02e60b1
Viciafaba <138016574+Viciafaba@users.noreply.github.com>
2026-03-05 09:14:04 +0100

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Base XLSX Reports',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Wsparcie generowania prawdziwych raportów XLSX bezpośrednio z Akcji Serwera',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
